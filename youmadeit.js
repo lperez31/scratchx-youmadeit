c672e9e8f67e8cddc3b57be5588e0e708397ecd3
Lior Perez <pertoul@gmail.com>
2016-11-25 15:46:14 +0100

    Test avec lib mqtt js simple

--- youmadeit.js
+++ youmadeit.js
@@ -1,4 +1,6 @@
 (function(ext) {
+    var alarm_went_off = false; // This becomes true after the alarm goes off
+
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -8,26 +10,31 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.get_temp = function(location, callback) {
-        // Make an AJAX call to the Open Weather Maps API
-        $.ajax({
-              url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
-              dataType: 'jsonp',
-              success: function( weather_data ) {
-                  // Got the data - parse it and return the temperature
-                  // temperature = weather_data['main']['temp'];
-                  callback(12);
-              }
-        });
+    ext.set_alarm = function(time) {
+       window.setTimeout(function() {
+           alarm_went_off = true;
+       }, time*1000);
+    };
+
+    ext.when_alarm = function() {
+       // Reset alarm_went_off if it is true, and return true
+       // otherwise, return false.
+       if (alarm_went_off === true) {
+           alarm_went_off = false;
+           return true;
+       }
+
+       return false;
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['R', 'current temperature in city %s', 'get_temp', 'Boston, MA'],
+            ['', 'run alarm after %n seconds', 'set_alarm', '2'],
+            ['h', 'when alarm goes off', 'when_alarm'],
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Weather extension', descriptor, ext);
+    ScratchExtensions.register('Alarm extension', descriptor, ext);
 })({});
